1c1761feca867e3374510f0fda53aa20b51a9597
Cris <111997390+iris-cmd22@users.noreply.github.com>
2023-09-01 13:29:23 +0200

    Update genitore.js

--- Progetto_ACP/root_genitore/genitore.js
+++ Progetto_ACP/root_genitore/genitore.js
@@ -1,5 +1,9 @@
 //Variabile globale per segnalare che l'iscrizione è stata approvata
-var iscrizione_approvata=0;
+var iscrizione_sottoposta=0;
+var iscrizione_accettata=0;
+var matricola_personale=0;
+var codicefiscalestud=0;
+var num_attivita=0;
 
 var main=function(){
 
@@ -25,10 +29,9 @@ var main=function(){
 
                 $cont=$("<div>");
 
-                //Label e input box per i campi da riempire
-                var $labelmatricola=$("<p class='labeltext'>").text("Matricola");
-                var $inputmatricola=$("<input type='number' placeholder='Inserisci la matricola dello studente...'>");
+                if(iscrizione_sottoposta==0){
 
+                //Label e input box per i campi da riempire
                 var $labelnome=$("<p class='labeltext'>").text("Nome");
                 var $inputnome=$("<textarea class='textbox' placeholder='Inserisci nome ...'>");
 
@@ -52,12 +55,11 @@ var main=function(){
                 $buttoniscrizione.on("click",function(){
 
                     //Sono stati compilati tutti i campi?
-                    if($inputmatricola.val()!="" && $inputnome.val()!="" && $inputcognome.val()!="" && $inputcodicefiscale.val()!="" && $inputdatanascita.val()!="" && $inputclasse.val()!="" ){
+                    if($inputnome.val()!="" && $inputcognome.val()!="" && $inputcodicefiscale.val()!="" && $inputdatanascita.val()!="" && $inputclasse.val()!="" ){
 
                         //Sì,tutto compilato
                         //Creo l'oggetto studente
-                        var stud={matricola: $inputmatricola.val(), 
-                            nome: $inputnome.val(), 
+                        var stud={nome: $inputnome.val(), 
                             cognome: $inputcognome.val(),
                             codicefiscale: $inputcodicefiscale.val(),
                             datanascita: $inputdatanascita.val(),
@@ -72,26 +74,21 @@ var main=function(){
                             data:stud
                         }).done(function(){
                             //Inserimento avvenuto con successo
-                            $("p.notify").text("Iscrizione effettuata").hide().fadeIn(800).fadeOut(3000);
-                            $inputmatricola.val("");
+                            $("p.notify").text("Iscrizione sottoposta").hide().fadeIn(800).fadeOut(3000);
                             $inputnome.val("");
                             $inputcognome.val("");
+                            codicefiscalestud=$inputcodicefiscale.val();
                             $inputcodicefiscale.val("");
                             $inputdatanascita.val("");
                             $inputclasse.val("");
                         })
 
-                        iscrizione_approvata=1;
+                        iscrizione_sottoposta=1;
 
                     }else{
                         //No, non tutti i campi sono stati compilati
                         $("p.notify").text("Compila tutti i campi!").hide().fadeIn(800).fadeOut(3000);
                     }
-                });
-
-                $inputmatricola.on("keypress",function(event){
-                    if(event.key=="Enter")
-                    $inputnome.focus();
                 });
 
                 $inputnome.on("keypress",function(event){
@@ -119,20 +116,69 @@ var main=function(){
                         $buttoniscrizione.focus();
                 });
 
-
-
-
                 //append degli elemnti html
-                $cont.append($labelmatricola).append($inputmatricola).append($labelnome).append($inputnome).append($labelcognome).append($inputcognome).append($labelcodicefiscale).append($inputcodicefiscale).append($labeldatanascita).append($inputdatanascita).append($labelclasse).append($inputclasse).append( $buttoniscrizione);
+                $cont.append($labelnome).append($inputnome).append($labelcognome).append($inputcognome).append($labelcodicefiscale).append($inputcodicefiscale).append($labeldatanascita).append($inputdatanascita).append($labelclasse).append($inputclasse).append( $buttoniscrizione);
                 $("main .content").append($cont);
-                
+
+            }else{
+
+                //Visualizza Iscrizione approvata o meno
+
+                 //GET Ajax all'url offerto da server_segreteria per l'ottenimento della lista di quadri
+                 $.getJSON("/getIscrizione", (iscrizioni)=>{
+
+                    iscrizioni.forEach((studente)=>{
+
+                        if(studente.codicefiscale==codicefiscalestud){
+                            
+                            var $matricolastud = $("<li class='listamatricolestudenti'>").text("Matricola "+studente.matricola);
+                            var $infoboxstud = $("<li class='infoboxstudente'>").text(studente.nome+" "+studente.cognome+" CF:"+studente.codicefiscale+" Nato il:"+studente.datanascita+" Classe:"+studente.classe);
+
+                            var stato_richiesta = studente.stato;
+
+
+                            //Qual è lo stato dell'iscrizione?
+                            if(stato_richiesta=="ASSEGNATO"){
+
+                                //Iscrizione effettuata con successo
+                                $("p.notify").text("Iscrizione effettuata con successo").hide().fadeIn(800).fadeOut(3000);
+                                $cont.append($matricolastud).append($infoboxstud);
+                                $("main .content").append($cont);
+                                matricola_personale=studente.matricola;
+                                iscrizione_accettata=1;
+
+                            }else if(stato_richiesta=="RIGETTATO"){
+
+                                //Iscrizione rigettata
+                                $("p.notify").text("Siamo spiacenti, la tua iscrizione è stata rigettata.").hide().fadeIn(800);
+                    
+
+                            }else if(stato_richiesta=="ATTESA"){
+
+                                //Iscrizione in attesa
+                                $("p.notify").text("Ancora un po' di pazienza, la tua iscrizione verrà elaborata al più presto.").hide().fadeIn(800);
+                    
+
+                            }
+
+                            
+                        }
+
+                    })
+                    
+                }).fail((jqXHR)=>{
+                    $cont.append($("<li class='error'>").text("Nessun quadro trovato!"));
+                })
+
+
+            }
 
             }else if($element.parent().is(":nth-child(2)")){
 
                 //Visualizza Quadri
                 $cont = $("<ul>");
 
-                if(iscrizione_approvata==0){
+                if(iscrizione_accettata==0){
 
                     $("p.notify").text("Effettua prima l'iscrizione!").hide().fadeIn(800).fadeOut(3000);
 
@@ -143,6 +189,8 @@ var main=function(){
 
                         pagelle.forEach((pagella)=>{
 
+                            if(pagella.studente.matricola==matricola_personale){
+                                
                             var $listidpagelle = $("<li class='listidprenotazione'>").text("ID: "+pagella.id);
                             var $infopagella = $("<li class='infopagella'>").text("Matricola: "+pagella.studente.matricola+" - "+pagella.quadrimestre+" quadrimestre - Anno scolastico: "+pagella.annoscolastico);
                             var $votoita = $("<li class='listvotoita'>").text("Italiano: "+pagella.materie.votoitaliano);
@@ -151,6 +199,9 @@ var main=function(){
                             var $votosto = $("<li class='listvotosto'>").text("Storia: "+pagella.materie.votostoria);
 
                             $cont.append($listidpagelle).append($infopagella).append($votoita).append($votomat).append($votoing).append($votosto);
+                        
+                            }
+
                         })
                         
                     }).fail((jqXHR)=>{
@@ -170,7 +221,7 @@ var main=function(){
 
                 $cont=$("<ul>");
 
-                if(iscrizione_approvata==0){
+                if(iscrizione_accettata==0){
 
                     $("p.notify").text("Effettua prima l'iscrizione!").hide().fadeIn(800).fadeOut(3000);
 
@@ -186,12 +237,8 @@ var main=function(){
                             var $div = $("<div class='d-flex gap-2'>")
                             //Descrizione dell'attività
                             var $actdesc = $("<li class='attivitadesc'>").text(attivita.descrizione);
-                            //Appendo il tipo della news per differenziarle e lo scrivo insieme alla data
-                            $cont.append($("<li class='"+attivita.tipo.toString().toLowerCase()+"'>").text(attivita.tipo+" "+attivita.data));
-
-                            //Appendo il resto degli elementi html
-                            $div.append($actdesc).append($button);
-                            $cont.append($div);
+                            //Appendo 
+                            $cont.append($("<li class='"+attivita.tipo.toString().toLowerCase()+"'>").text(attivita.tipo+" "+attivita.data)).append($actdesc);
 
                         });
 
@@ -214,8 +261,49 @@ var main=function(){
     
 }
 
-//Notifica approvazione della pagella
+//Notifica nuove attività
+var check = function(firstcall){
 
+    //GET Ajax delle iscrizioni in attesa
+    $.getJSON("/getAttivita", (attivitas)=>{
+
+        //Se è la prima chiamata alla funzione, pongo il contatore di iscrizioni pari alla lunghezza dell'array di iscrizioni
+        if(firstcall){
+
+            num_attivita=attivitas.length;
+
+        }else if(num_attivita!=attivitas.length){
+
+            //Se il contatore è cambiato invio la notifica
+            num_attivita=attivitas.length;
+            var el = document.querySelector(".active");
+
+            if(iscrizione_accettata!=0){
+                //Se mi trovo già sul primo tab triggero il click fittiziamente
+                if(el.getAttribute("id")=="attivitatab"){
+                    $(el).trigger("click");
+                    $(".alert").text("Nuova attivita'!");
+                    $(".alert").on("click",()=>{
+                        $(".alert").text("");
+                    })
+
+                }else{
+                    //Se invece mi trovo su un'altro tab notifico la modifica
+                    $(".alert").text("Aggiornate le attivita");
+                    $(".alert").on("click", ()=>{
+
+                        $(".alert").text("");
+                    });
+
+                }
+            }
+            
+
+        }
+
+    });
+
+}
 
 
 
